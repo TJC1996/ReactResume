9b9753d0847034a2b5925b3b6398d81a08e6cfb6
TJC1996 <tonyclark1996@gmail.com>
2023-08-27 13:11:05 -0700

    first commit

---
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -0,0 +1,155 @@
+// import {FC, memo, useCallback, useMemo, useState} from 'react';
+
+// interface FormData {
+//   name: string;
+//   email: string;
+//   message: string;
+// }
+
+// const ContactForm: FC = memo(() => {
+//   const defaultData = useMemo(
+//     () => ({
+//       name: '',
+//       email: '',
+//       message: '',
+//     }),
+//     [],
+//   );
+
+//   const [data, setData] = useState<FormData>(defaultData);
+
+//   const onChange = useCallback(
+//     <T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
+//       const {name, value} = event.target;
+
+//       const fieldData: Partial<FormData> = {[name]: value};
+
+//       setData({...data, ...fieldData});
+//     },
+//     [data],
+//   );
+
+//   const handleSendMessage = useCallback(
+//     async (event: React.FormEvent<HTMLFormElement>) => {
+//       event.preventDefault();
+//       /**
+//        * This is a good starting point to wire up your form submission logic
+//        * */
+//       console.log('Data to send: ', data);
+//     },
+//     [data],
+//   );
+
+//   const inputClasses =
+//     'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-blue-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
+
+//   return (
+//     <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={handleSendMessage}>
+//       <input className={inputClasses} name="name" onChange={onChange} placeholder="Name" required type="text" />
+//       <input
+//         autoComplete="email"
+//         className={inputClasses}
+//         name="email"
+//         onChange={onChange}
+//         placeholder="Email"
+//         required
+//         type="email"
+//       />
+//       <textarea
+//         className={inputClasses}
+//         maxLength={250}
+//         name="message"
+//         onChange={onChange}
+//         placeholder="Message"
+//         required
+//         rows={6}
+//       />
+//       <button
+//         aria-label="Submit contact form"
+//         className="w-max rounded-full border-2 border-blue-600 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-stone-800"
+//         type="submit">
+//         Send Message
+//       </button>
+//     </form>
+//   );
+// });
+
+// ContactForm.displayName = 'ContactForm';
+// export default ContactForm;
+
+
+import { FC, memo, useCallback, useMemo, useState } from 'react';
+import emailjs from 'emailjs-com'; // Step 1: Import emailjs-com
+
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const ContactForm: FC = memo(() => {
+  const defaultData = useMemo(() => ({ name: '', email: '', message: '' }), []);
+
+  const [data, setData] = useState<FormData>(defaultData);
+  const [buttonText, setButtonText] = useState<string>("Send Message"); // To control button text
+
+  const onChange = useCallback(<T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
+    const { name, value } = event.target;
+    const fieldData: Partial<FormData> = { [name]: value };
+    setData({ ...data, ...fieldData });
+  }, [data]);
+
+  const handleSendMessage = useCallback(async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setButtonText('Sending...'); // Update button text when sending
+
+    const serviceID = 'default_service';
+    const templateID = 'template_o1n56jd';
+
+    emailjs.sendForm(serviceID, templateID, event.target as HTMLFormElement)
+      .then(() => {
+        setButtonText('Send Message');
+        alert('Sent!');
+      }, (err) => {
+        setButtonText('Send Message');
+        alert(JSON.stringify(err));
+      });
+
+  }, []);
+
+  const inputClasses = 'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-blue-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
+
+  return (
+    <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={handleSendMessage}>
+      <input className={inputClasses} name="from_name" onChange={onChange} placeholder="Name" required type="text" />
+      <input
+        autoComplete="email"
+        className={inputClasses}
+        name="email_id"
+        onChange={onChange}
+        placeholder="Email"
+        required
+        type="email"
+      />
+      <textarea
+        className={inputClasses}
+        maxLength={250}
+        name="message"
+        onChange={onChange}
+        placeholder="Message"
+        required
+        rows={6}
+      />
+      <button
+        aria-label="Submit contact form"
+        className="w-max rounded-full border-2 border-blue-600 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-stone-800"
+        type="submit">
+        {buttonText}
+      </button>
+    </form>
+  );
+});
+
+ContactForm.displayName = 'ContactForm';
+export default ContactForm;
---
+++ src/data/data.tsx
@@ -0,0 +1,386 @@
+import {
+  AcademicCapIcon,
+  ArrowDownTrayIcon,
+  BuildingOffice2Icon,
+  CalendarIcon,
+  FlagIcon,
+  MapIcon,
+  SparklesIcon,
+} from '@heroicons/react/24/outline';
+
+import GithubIcon from '../components/Icon/GithubIcon';
+import InstagramIcon from '../components/Icon/InstagramIcon';
+import LinkedInIcon from '../components/Icon/LinkedInIcon';
+import emailjs from 'emailjs-com';
+// import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
+// import TwitterIcon from '../components/Icon/TwitterIcon';
+import heroImage from '../images/forest.png';
+import porfolioImage1 from '../images/portfolio/portfolio-1.png';
+import porfolioImage2 from '../images/portfolio/portfolio-2.png';
+// import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
+// import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
+// import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
+// import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+// import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
+// import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
+// import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
+// import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
+// import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+import profilepic from '../images/profilepic.png';
+import testimonialImage from '../images/sandy.png';
+import {
+  About,
+  ContactSection,
+  ContactType,
+  Hero,
+  HomepageMeta,
+  PortfolioItem,
+  SkillGroup,
+  Social,
+  TestimonialSection,
+  TimelineItem,
+} from './dataDef';
+
+emailjs.init('o9FZ4o6_bTNsmBLBD');
+
+/**
+ * Page meta data
+ */
+export const homePageMeta: HomepageMeta = {
+  title: 'Anthony Clark React Resume',
+  description: "Anthony Clark react resume website.",
+};
+
+/**
+ * Section definition
+ */
+export const SectionId = {
+  Hero: 'hero',
+  About: 'about',
+  Contact: 'contact',
+  Portfolio: 'portfolio',
+  Resume: 'resume',
+  Skills: 'skills',
+  Stats: 'stats',
+  Testimonials: 'testimonials',
+} as const;
+
+export type SectionId = (typeof SectionId)[keyof typeof SectionId];
+
+/**
+ * Hero section
+ */
+export const heroData: Hero = {
+  imageSrc: heroImage,
+  name: `I'm Anthony Clark.`,
+  description: (
+    <>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+        I'm a San Diego based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
+        at <strong className="text-stone-100">Fortrea</strong> helping build mobile clincial trials for Labcorp patient studies.
+      </p>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+        In my free time time, you can catch me skateboarding at the <strong className="text-stone-100">skatepark</strong>,
+        chasing good <strong className="text-stone-100">snowstorms</strong>, or exploring beautiful{' '}
+        <strong className="text-stone-100">hikes</strong>.
+      </p>
+    </>
+  ),
+  actions: [
+    {
+      href: '/assets/Resume.pdf',
+      text: 'Resume',
+      primary: true,
+      Icon: ArrowDownTrayIcon,
+    },
+    {
+      href: `#${SectionId.Contact}`,
+      text: 'Contact',
+      primary: false,
+    },
+  ],
+};
+
+/**
+ * About section
+ */
+export const aboutData: About = {
+  profileImageSrc: profilepic,
+  description: `A highly motivated and dedicated Software Engineer seeking a full-time position where I can contribute to the success of a dynamic company and further develop my skills in the Software Technology field.`,
+  aboutItems: [
+    {label: 'Location', text: 'San Diego, CA', Icon: MapIcon},
+    {label: 'Age', text: '27', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Italian / Irish / Polish', Icon: FlagIcon},
+    {label: 'Interests', text: 'Skateboarding, Snowboarding, Hiking', Icon: SparklesIcon},
+    {label: 'Study', text: 'San Diego State University', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Labcorp, Fortrea', Icon: BuildingOffice2Icon},
+  ],
+};
+
+/**
+ * Skills section
+ */
+export const skills: SkillGroup[] = [
+  {
+    name: 'Spoken languages',
+    skills: [
+      {
+        name: 'English',
+        level: 10,
+      },
+      {
+        name: 'Spanish',
+        level: 2,
+      },
+    ],
+  },
+  {
+    name: 'Frontend development',
+    skills: [
+      {
+        name: 'React',
+        level: 9,
+      },
+      {
+        name: 'Material UI',
+        level: 8,
+      },
+      {
+        name: 'Bootstrap',
+        level: 8,
+      },
+    ],
+  },
+  {
+    name: 'Backend development',
+    skills: [
+      {
+        name: 'Node.js',
+        level: 9,
+      },
+      {
+        name: 'Express.js',
+        level: 8,
+      },
+      {
+        name: 'MongoDB',
+        level: 8,
+      },
+    ],
+  },
+  {
+    name: 'Mobile development',
+    skills: [
+      {
+        name: 'React Native',
+        level: 9,
+      },
+      {
+        name: 'Android',
+        level: 8,
+      },
+      {
+        name: 'iOS',
+        level: 8,
+      },
+    ],
+  },
+];
+
+/**
+ * Portfolio section
+ */
+export const portfolioItems: PortfolioItem[] = [
+  {
+    title: 'Crochet Nation',
+    description: 'A fully live eCommerce store',
+    url: 'https://crochetnation.org/',
+    image: porfolioImage1,
+  },
+  {
+    title: 'Yelp Camp',
+    description: 'A location based Campground Review App.',
+    url: 'https://yelpcamprender.onrender.com/',
+    image: porfolioImage2,
+  },
+  // {
+  //   title: 'Project title 3',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage3,
+  // },
+  // {
+  //   title: 'Project title 4',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage4,
+  // },
+  // {
+  //   title: 'Project title 5',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage5,
+  // },
+  // {
+  //   title: 'Project title 6',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage6,
+  // },
+  // {
+  //   title: 'Project title 7',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage7,
+  // },
+  // {
+  //   title: 'Project title 8',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage8,
+  // },
+  // {
+  //   title: 'Project title 9',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage9,
+  // },
+  // {
+  //   title: 'Project title 10',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage10,
+  // },
+  // {
+  //   title: 'Project title 11',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage11,
+  // },
+];
+
+/**
+ * Resume section -- TODO: Standardize resume contact format or offer MDX
+ */
+export const education: TimelineItem[] = [
+  {
+    date: 'December 2019',
+    location: 'San Diego, CA',
+    title: 'San Diego State University',
+    content: <p>During my college years, I demonstrated adaptability and perseverance by moving across the country and successfully building a strong academic record and network from scratch.</p>,
+  },
+  {
+    date: 'Decmeber 2019',
+    location: 'Fowler School of Business',
+    title: 'Management Information Systems',
+    content: <p>My academic journey at SDSU has not just equipped me with the knowledge of Information Systems but also honed a myriad of skills that I am eager to apply in the professional realm. Whether it's database management, system design, programming, or data analytics, I am confident in my abilities to contribute, innovate, and excel.</p>,
+  },
+];
+
+export const experience: TimelineItem[] = [
+  {
+    date: 'April 2023 - Present',
+    location: 'Crochet Nation',
+    title: 'Senior UX Engineer',
+    content: (
+      <ul>
+      <li>- Developed a responsive ecommerce store for Crochet Nation using Commerce.js, Stripe, and React.</li>
+      <li>- Soley designed, developed, and deployed the whole application on my own.</li>
+      <li>- Implemeneted and configured best practices for accepting online payments.</li>
+      <li>- Created a fully live checkout system which, accepts real time payments, calucate taxes, and fulfill orders.</li>
+      </ul>
+    ),
+  },
+  {
+    date: 'January 2021 - Present',
+    location: 'Labcorp Covance',
+    title: 'Software Engineer',
+    content: (
+      <ul>
+      <li>- Developed clinical trials utilizing languages such as JavaScript, HTML5, and CSS3, ensuring seamless user experience and increased customer satisfaction.</li>
+      <li>- Collaborated with cross-functional teams, including UI/UX designers, product managers, and back-end developers, to deliver cohesive and comprehensive web solutions for clients.</li>
+      <li>- Configured and managed Mobile Device Management (MDM) policies to ensure the security and compliance of mobile applications across various devices, protecting sensitive clinical trial data and adhering to industry standards.</li>
+      <li>- Created custom widgets to fulfill project requirements</li>
+      </ul>
+    ),
+  },
+  {
+    date: 'June 2020 - January 2021',
+    location: 'SnapIot',
+    title: 'Web Application Developer',
+    content: (
+      <ul>
+      <li>- Played a pivotal role in the rapid growth of SnapIot as a leading platform-as-a-service provider for mobile-connected, self-service clinical trial solutions during the Covid-19 pandemic, ensuring timely delivery of critical applications.</li>
+      <li>- Developed and maintained high-performance web applications to streamline clinical trials, utilizing the SnapClinical platform, contributing to increased efficiency and adaptability during the pandemic.</li>
+      <li>- Adapted to remote work environments and maintained clear communication with team members, leveraging Agile methodologies and tools like Jira and Git to effectively manage project progress and consistently meet tight deadlines.</li>
+      </ul>
+    ),
+  },
+];
+
+/**
+ * Testimonial section
+ */
+export const testimonial: TestimonialSection = {
+  imageSrc: testimonialImage,
+  testimonials: [
+    {
+      name: 'Susan Sitar',
+      text: 'I am extremely happy with Crochet Nation, our small business can now go to the next step with our online store!',
+      image: 'https://i.ibb.co/Bgs7Hf6/mom2.png',
+    },
+    {
+      name: 'Patricia Rivero',
+      text: 'I worked with Anthony to revamp a e-commerce site. Anthony is really open to critiques and makes the effort to implement design changes to his work. It really helps with creating and implementing up to date designs that are aligned with my user research.',
+      image: 'https://i.ibb.co/qmyvSmn/Patty.png',
+    },
+    {
+      name: 'Stella Clark',
+      text: 'It was a breeze to help design Crochet Nation, I am glad to have had the opportunity to work along side Anthony.',
+      image: 'https://i.ibb.co/4pdpFDK/Customer3.png',
+    },
+  
+  ],
+};
+
+/**
+ * Contact section
+ */
+
+export const contact: ContactSection = {
+  headerText: 'Contact Me',
+  description: 'Feel free to reach out below with any business inquries or questions.',
+  items: [
+    {
+      type: ContactType.Email,
+      text: 'tonyclark1996@gmail.com',
+      href: 'mailto:tonyclark1996@gmail.com',
+    },
+    {
+      type: ContactType.Location,
+      text: 'San Diego, CA',
+      href: 'https://www.google.com/maps/place/Ocean+Beach,+San+Diego,+CA/@32.7461888,-117.2856543,13z/data=!3m1!4b1!4m6!3m5!1s0x80deaa37b72c78ff:0xccd2ea1ce1f90202!8m2!3d32.7494988!4d-117.2470353!16zL20vMDNyN2oz?entry=ttu',
+    },
+    {
+      type: ContactType.Instagram,
+      text: '@tony__clark',
+      href: 'https://www.instagram.com/tony__clark/',
+    },
+    {
+      type: ContactType.Github,
+      text: 'tjc1996',
+      href: 'https://github.com/tjc1996',
+    },
+  ],
+};
+
+/**
+ * Social items
+ */
+export const socialLinks: Social[] = [
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tjc1996'},
+  // {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/anthony-clark-6a0543162/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/tony__clark/'},
+  // {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+];
