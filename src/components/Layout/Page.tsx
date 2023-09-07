44f2340563c8912cc54fd7ae0b42a5231acd3fc2
TJC1996 <tonyclark1996@gmail.com>
2023-09-07 12:31:54 -0700

    Updated Page.tsx for Deployment

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,56 +1,40 @@
-import { NextPage } from 'next';
+import {NextPage} from 'next';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { memo, PropsWithChildren } from 'react';
+import {useRouter} from 'next/router';
+import {memo, PropsWithChildren} from 'react';
 
-import { HomepageMeta } from '../../data/dataDef';
+import {HomepageMeta} from '../../data/dataDef';
 
-const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({ children, title, description }) => {
-  const { asPath: pathname } = useRouter();
+const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, description}) => {
+  const {asPath: pathname} = useRouter();
 
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta content={description} name="description" />
+        <meta name="description" content={description} />
 
-        {/* Canonical link */}
-        <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
+        <link key="canonical" rel="canonical" href={`https://reactresume.com${pathname}`} />
 
-        {/* Favicon and App Icons */}
-        <link href="/favicon.png" rel="icon" sizes="any" />
-        <link href="/favicon.png" rel="icon" type="image/svg+xml" />
-        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
-        <link href="/site.webmanifest" rel="manifest" />
+        <link rel="icon" sizes="any" href="/favicon.png" />
+        <link type="image/svg+xml" rel="icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
 
-        {/* Mobile Responsiveness */}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content={`https://reactresume.com${pathname}`} />
 
-        {/* Locale and Content Language */}
-        <meta property="og:locale" content="en_US" />
-        <meta name="content-language" content="en" />
-
-        {/* Open Graph */}
-        <meta content={title} property="og:title" />
-        <meta content={description} property="og:description" />
-        <meta content={`https://reactresume.com${pathname}`} property="og:url" />
-        <meta content="https://reactresume.com/your-image.jpg" property="og:image" />
-
-        {/* Twitter Cards */}
-        <meta content={title} name="twitter:title" />
-        <meta content={description} name="twitter:description" />
-        <meta content="https://reactresume.com/your-image.jpg" name="twitter:image" />
-
-        {/* Structured Data */}
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:title" content={title} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
-              "@context": "http://schema.org",
-              "@type": "Person",
+              "@context": "http://anthonyclark1996.com",
+              "@type": "Anthony Clark",
               "name": "Anthony Clark",
               "jobTitle": "Software Engineer",
-              // Add more attributes as needed
             }),
           }}
         />
