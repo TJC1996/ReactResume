e37344df2ce37f0f6f246c919db6caf844348318
TJC1996 <101535962+TJC1996@users.noreply.github.com>
2024-02-01 14:40:25 -0800

    Update data.tsx

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -219,7 +219,7 @@ export const portfolioItems: PortfolioItem[] = [
     title: 'Notes App',
     description: 'Notes App for Web',
     url: 'https://notes-app-port.vercel.app/',
-    image: porfolioImage6,
+    image: portfolioImage6,
   },
   // {
   //   title: 'Project title 7',
