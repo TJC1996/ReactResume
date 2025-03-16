1ae0b613009af5c02dc4e251de40d09684b64043
TJC1996 <101535962+TJC1996@users.noreply.github.com>
2025-03-16 19:22:13 -0400

    Update data.tsx
    Updated Resume on Homepage

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -83,7 +83,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: '/assets/Resume2024.pdf',
+      href: '/assets/ResumeMarch25.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
